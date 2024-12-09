b15c9162344fb4ab678c4c63903a9ee835152be8
Kyle Baker <kyle@earths.center>
2024-12-09 09:40:12 -0500

    Stash

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
--- This is (mostly) copied from kickstart.nvim
-
 local ensure_installed = {
  'bash',
  'clojure',
@@ -19,19 +17,14 @@ local ensure_installed = {
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-  -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+  main = 'nvim-treesitter.configs',
+  -- See `:help nvim-treesitter`
   opts = {
     ensure_installed = ensure_installed,
-    -- Autoinstall languages that are not installed
     auto_install = true,
     highlight = {
       enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
     },
-    indent = { enable = true, disable = { 'ruby' } },
+    indent = { enable = true },
   },
 }
